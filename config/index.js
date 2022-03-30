906a1ef798fec1da5b80cf52ee2ca326fd5cd07b
Vanesa Medina <mv382038@gmail.com>
2022-03-30 14:56:07 -0300

    Delete index.js

--- config/index.js
+++
@@ -1,8 +0,0 @@
-module.exports = {
-    HOSTNAME : "cluster0.vczmx.mongodb.net",
-    SCHEMA : "mongodb+srv",
-    USER: "vane",
-    PASSWORD: "MfQWE7MTU6nhskn",
-    DATABASE: "ecommerce",
-    OPTIONS: "retryWrites=true&w=majority"
-}
